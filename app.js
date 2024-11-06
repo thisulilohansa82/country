745109fc3bd8ef63c9a056c1f30da1a6913beefb
thisulilohansa82 <thisulilohansa082@gmail.com>
2024-11-06 23:26:01 +0530

    Main

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ async function loadItems() {
             <div class="col-lg-4">
               <img src=${element.flags.png} alt="">
               <h2 class="fw-normal">${element.name.common}</h2>
-              <p>${element.name.official}</p>
+              <p>${element.name.nativeNameofficial}</p>
               <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
             </div><!-- /.col-lg-4 -->
         `;
@@ -23,5 +23,38 @@ async function loadItems() {
     console.log(body);
 
     document.getElementById("row").innerHTML=body;
+    
+}
+
+function searchCountry(){
+    console.log("Search!!");
+    let txtSearch = document.getElementById("countryInput").value;
+
+    fetch(`https://restcountries.com/v3.1/name/${txtSearch}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        let body="";
+        data.forEach(element => {
+            body+=`
+            
+            <div class="bg-body-tertiary p-5 rounded" style="background-image: url(${element.flags.png}); background-repeat: no-repeat; background-size: cover;">
+          <div class="col-sm-8 py-5 mx-auto">
+            <h1 class="display-5 fw-normal text-white">${element.name.common}</h1>
+            <p class="fs-5 text-white">${element.flags.alt}</p>
+            <h2 class="text-white">${element.capital[0]}</h2>
+            <p>From the top down, you'll see a dark navbar, light navbar and a responsive navbar—each with offcanvases built in. Resize your browser window to the large breakpoint to see the toggle for the offcanvas.</p>
+            <p>
+              <a class="btn btn-primary" href="/docs/5.3/components/navbar/#offcanvas" role="button">Learn more about offcanvas navbars &raquo;</a>
+            </p>
+        </div>
+        </div>
+            
+            `
+        });
+
+        document.getElementById("row").innerHTML=body;
+        
+    })
     
 }
